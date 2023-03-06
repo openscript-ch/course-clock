aba76f26a6360fa937cd2014ff69162e602310a0
micael <micael.staeubli@gmail.com>
2023-03-06 10:37:04 +0100

    chore: ready to start

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { MantineProvider, Text } from '@mantine/core';
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text>Welcome to Mantine!</Text>
+      <p>hello world</p>
     </MantineProvider>
   );
 }
