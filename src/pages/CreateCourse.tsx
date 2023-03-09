0f9204a718f04f1e7d9df46a9a51dd5f6620ee1e
micael <micael.staeubli@gmail.com>
2023-03-09 14:39:12 +0100

    chore: Page Layout structure (Header/Navbar)

--- src/pages/CreateCourse.tsx
+++ src/pages/CreateCourse.tsx
@@ -1,12 +1,35 @@
-import '../App.css'
+import { AppShell, Navbar, Header, Title,ThemeIcon, Tabs,Input, Textarea, Button, Group  } from '@mantine/core';
+import { useState, useRef } from 'react';
+
 interface CreateCourseProps {
   setShowHomepageContent: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export function CreateCourse({ setShowHomepageContent }: CreateCourseProps){
+  const [value, setValue] = useState<Date | null>(null);
+  const [value2, setValue2] = useState<Date | null>(null);
+  const openRef = useRef<() => void>(null)
+
  return(
-  <div>
-   <p style={{color: 'black'}}>welcome to Create page </p>
-  </div>
- )
+  <AppShell
+      padding="md"
+      header={
+      <Header height={100} p="xs" className='header'>
+      </Header>
+      }
+
+      navbar={
+      <Navbar width={{ base: 100 }} p="xs" className='navbar'>
+      </Navbar>}
+
+
+      styles={(theme) => ({
+        main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+  })}>
+
+    {}
+
+  </AppShell>
+
+  );
 }
