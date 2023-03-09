437f148f7641eea5e63a228c1b5ae1e18528f5ea
micael <micael.staeubli@gmail.com>
2023-03-09 15:38:26 +0100

    chore: Layout styling

--- src/pages/CreateCourse.tsx
+++ src/pages/CreateCourse.tsx
@@ -1,25 +1,51 @@
 import { AppShell, Navbar, Header, Title,ThemeIcon, Tabs,Input, Textarea, Button, Group  } from '@mantine/core';
-import { useState, useRef } from 'react';
+import { IconPlus, IconFile, IconPdf, IconSettings2, IconClock } from '@tabler/icons-react';
+import '../App.css'
 
 interface CreateCourseProps {
   setShowHomepageContent: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export function CreateCourse({ setShowHomepageContent }: CreateCourseProps){
-  const [value, setValue] = useState<Date | null>(null);
-  const [value2, setValue2] = useState<Date | null>(null);
-  const openRef = useRef<() => void>(null)
-
  return(
   <AppShell
       padding="md"
       header={
       <Header height={100} p="xs" className='header'>
+      {<h2>courseCL 
+          <ThemeIcon radius={360} size={'lg'} color={"orange"}>
+            <IconClock></IconClock>
+          </ThemeIcon>CK
+        </h2>}
       </Header>
       }
 
       navbar={
       <Navbar width={{ base: 100 }} p="xs" className='navbar'>
+        {
+          <div className='nav-Bar'> 
+
+            <ThemeIcon className='main-Option' style={{marginTop: '1rem'}} radius={360} size={'lg'} color={"orange"}>
+              <IconPlus/>
+            </ThemeIcon>
+            <br />
+
+            <ThemeIcon className='main-Option' variant={"outline"} radius={360} size={'lg'} color={"dark"}>
+              <IconFile/>
+            </ThemeIcon>
+            <br />
+
+            <ThemeIcon className='main-Option' variant={"outline"} radius={360} size={'lg'} color={"dark"}>
+              <IconPdf/>
+            </ThemeIcon>
+            <br />
+
+            <ThemeIcon className='setting-Icon' variant={"outline"} radius={360} size={'lg'} color={"dark"}>
+              <IconSettings2/>
+            </ThemeIcon>
+            
+          </div> 
+        }
       </Navbar>}
 
 
