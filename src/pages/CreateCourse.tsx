5f4be03f71bd4ed3aa03e5b6eae8b2436f3ee121
micael <micael.staeubli@gmail.com>
2023-03-14 08:53:29 +0100

    chore: CreateCourse linked with EditCourse

--- src/pages/CreateCourse.tsx
+++ src/pages/CreateCourse.tsx
@@ -1,5 +1,6 @@
-import { AppShell, Navbar, Header, Title,ThemeIcon, Tabs,Input, Textarea, Button, Group  } from '@mantine/core';
+import { AppShell, Navbar, Header, ThemeIcon  } from '@mantine/core';
 import { IconPlus, IconFile, IconPdf, IconSettings2, IconClock } from '@tabler/icons-react';
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import '../App.css'
 import { CreateComponents } from '../components/CreateComponents';
 
@@ -31,9 +32,11 @@ export function CreateCourse({ setShowHomepageContent }: CreateCourseProps){
             </ThemeIcon>
             <br />
 
+            <Link to='/edit-course'>
             <ThemeIcon className='main-Option' variant={"outline"} radius={360} size={'lg'} color={"dark"}>
               <IconFile/>
             </ThemeIcon>
+            </Link>
             <br />
 
             <ThemeIcon className='main-Option' variant={"outline"} radius={360} size={'lg'} color={"dark"}>
--- src/pages/EditCourse.tsx
+++ src/pages/EditCourse.tsx
@@ -1,12 +1,16 @@
 import '../App.css'
+import { useNavigate } from "react-router-dom";
+
 interface EditCourseProps {
   setShowHomepageContent: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export function EditCourse({ setShowHomepageContent }: EditCourseProps){
- return(
-  <div>
-   <p style={{color: 'black'}}>welcome to Edit </p>
-  </div>
- )
+export function EditCourse({ setShowHomepageContent }: EditCourseProps) {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Edit Course</h1>
+    </div>
+  );
 }
