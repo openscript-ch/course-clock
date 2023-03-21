fa70417ae551223e906a71ffcc903bebc14dc47d
micael <micael.staeubli@gmail.com>
2023-03-21 15:51:54 +0100

    chore: edit and save Course components

--- src/components/EditComponents.tsx
+++ src/components/EditComponents.tsx
@@ -1,26 +1,70 @@
 import { Tabs, Button,Group, Box, TextInput,Textarea, ThemeIcon} from '@mantine/core';
-import { useState } from 'react';
-import { useForm } from '@mantine/form';;
+import { useState }  from 'react';
+import { useForm }   from '@mantine/form';;
 import { DateInput } from '@mantine/dates';
 import '../App.css'
-import { AddMaterial } from './AddMaterial';
-import { CourseInformation, updateCourseInformation } from '../data/CourseInformation';
+import { updateCourseInformation } from '../data/CourseInformation';
 import { TimeInput } from '@mantine/dates';
-import { IconEdit, IconPlus } from '@tabler/icons-react';
+import { IconEdit } from '@tabler/icons-react';
 import { newListInfo } from './CreateComponents';
-import { Segment } from './Segment';
+import { Segment, SegmentProps } from './Segment';
 
 
+export function EditComponents(props: SegmentProps){
 
-export function EditComponents(){
 const [activeTab, setActiveTab] = useState<string | null>('allgemein');
+const allgemeinInfo = newListInfo[newListInfo.length - 1];
+
+const onSubmit = () => {
+  allgemeinInfo.Titel = form.values.Titel
+  allgemeinInfo.Autor = form.values.Autor
+  allgemeinInfo.dateStart = form.values.dateStart
+  allgemeinInfo.dateEnd = form.values.dateEnd
+  console.log(newListInfo)
+};
+
+const handleFormsSubmit = (event:any) => {
+  event.preventDefault()
+  form.onSubmit(onSubmit)();
+}
+const [selectedSegmentId, setSelectedSegmentId] = useState<number | null>(null)
+
+const onSegmentClick = (id: number) => {
+  const foundSegment = newListInfo.find(obj => {
+    return obj.id === id;
+  });
+  formSegment.setValues({
+    TitelSegment: foundSegment.TitelSegment,
+    startTime: foundSegment.startTime,
+    endTime: foundSegment.endTime,
+    target: foundSegment.target,
+    procedure: foundSegment.procedure,
+    materials: foundSegment.materials,
+  })
+  setSelectedSegmentId(id)
+};
+
+const onSegmentSave = () => {
+  let findSegment = newListInfo.find(obj => {
+    return obj.id === selectedSegmentId
+  })
+  findSegment.TitelSegment = formSegment.values.TitelSegment
+  findSegment.startTime = formSegment.values.startTime
+  findSegment.endTime = formSegment.values.endTime
+  findSegment.target = formSegment.values.target
+  findSegment.procedure = formSegment.values.procedure
+  findSegment.materials = formSegment.values.materials
+  formSegment.reset();
+  console.log(findSegment)
+  console.log(newListInfo)
+}
 
 const form = useForm({
   initialValues: {
-    Titel: '',
-    Autor: '',
-    dateStart: '',
-    dateEnd: '', 
+    Titel:     allgemeinInfo.Titel,
+    Autor:     allgemeinInfo.Autor,
+    dateStart: allgemeinInfo.dateStart,
+    dateEnd:   allgemeinInfo.dateEnd, 
   },
 
   validate: {
@@ -51,17 +95,16 @@ const formSegment = useForm({
 return(
   <>
     <div className='titel-Section'>
-      <h1 className='titel-Erstellen'> <span className='teko'>KURS</span> erstellen</h1>
+      <h1 className='titel-Erstellen'> <span className='teko'>KURS</span> editieren </h1>
       <p className='description'>hier <span className='teko'>KURS</span> Information bearbeiten</p>
     </div>
     <hr />
-    <form>
-      <Tabs variant="outline" defaultValue="gallery" className='tabs' value={activeTab} onTabChange={setActiveTab} >  
+    <form onSubmit={handleFormsSubmit}>
+      <Tabs variant="outline" defaultValue="gallery" className='tabs' value={activeTab} onTabChange={setActiveTab} >
         <Tabs.List>
          <Tabs.Tab value="allgemein">Allgemein</Tabs.Tab>
           <Tabs.Tab value="segmente">Segmente</Tabs.Tab>
-          <Tabs.Tab value="unterlagen">Materialen/Unterlagen</Tabs.Tab>
-          <Button type='submit' color={'green'} ml="auto">erstellen</Button>
+          <Button type='submit' color={'green'} ml="auto">speichern</Button>
         </Tabs.List>
       <br />
       <Tabs.Panel value="allgemein" pt="xs" >
@@ -113,21 +156,23 @@ return(
             icon={<IconEdit/>}
           />
 
-          <TimeInput
-            withAsterisk
-            label="Startzeizpunkt"
-            placeholder="Zeit"
-            {...formSegment.getInputProps('startTime')}
-            icon={<IconEdit/>}
-          />
+            <TimeInput
+              withAsterisk
+              label="Startzeizpunkt"
+              name='startTime'
+              placeholder="Zeit"
+              {...formSegment.getInputProps('startTime')}
+              icon={<IconEdit/>}
+            />
 
-          <TimeInput
-            withAsterisk
-            label="Endzeitpunkt"
-            placeholder="Zeit"
-            {...formSegment.getInputProps('endTime')}
-            icon={<IconEdit/>}
-          />
+            <TimeInput
+              withAsterisk
+              label="Endzeitpunkt"
+              name='endTime'
+              placeholder="Zeit"
+              {...formSegment.getInputProps('endTime')}
+              icon={<IconEdit/>}
+            />
 
           <Textarea
             name='target'
@@ -156,15 +201,12 @@ return(
         </Box>
         <div style={{display: 'flex', gap: '3rem', flexWrap: 'wrap'}}>
           {newListInfo.slice(0,-1).map((info, index) => (
-           <Segment key={index} segmentData={info}/>
-          ))}
+           <Segment key={index} segmentData={info} onClick={() => onSegmentClick(info.id)} />))}
         </div>
        </div>
+       <br />
+       <Button  onClick={onSegmentSave} color={'orange'}>Segment speichern</Button>
       <br />
-      </Tabs.Panel>
-
-      <Tabs.Panel value="unterlagen" pt="xs">
-        <AddMaterial></AddMaterial>
       </Tabs.Panel>
       </Tabs>
     </form>
