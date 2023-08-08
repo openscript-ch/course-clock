8598746b0528669b95eedec71ed85e02134c44dc
micael <micael.staeubli@gmail.com>
2023-08-08 11:30:11 +0200

    refactor: project react-router

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -10,11 +10,23 @@ import { IconPlus, IconFile, IconClock } from '@tabler/icons-react'
 import { BrowserRouter as Router,Route,Routes,Link } from 'react-router-dom'
 import { ExportPDF } from './exportPDF'
 
-export default function Homepage() {
-  const [showHomepageContent, setShowHomepageContent] = useState(true)
+function CourseClockRouter(){
+  return(
+  <Router>
+    <Routes>
+      <Route path='/' element={<Homepage/>} />
+      <Route path='/create-course' element={<CreateCourse/>} />
+      <Route path='/my-courses' element={<MyCourses/>}/>
+      <Route path='/edit-course' element={<EditCourse/>}/>
+      <Route path='/course-view' element={<CourseViewLayout/>}/>
+      <Route path='/course-pdf' element={<ExportPDF/>}/>
+    </Routes>
+  </Router>
+  );
+}
+
+function Homepage() {
     return (
-      <Router>
-        {showHomepageContent &&  (
         <div className='home-Page'>
           <h1 className='project-Name'>
             course<br />
@@ -25,16 +37,15 @@ export default function Homepage() {
             </span>
           </h1>
 
-
           <div className='options'>
-            <Link to='/create-course' className='options-Settings' style={{ backgroundColor:'#fd7e14', width: '30vw', marginBottom: '2rem',  height: '5.5rem' }} onClick={() => setShowHomepageContent(false)}>
+            <Link to='/create-course' className='options-Settings' style={{ backgroundColor:'#fd7e14', width: '30vw', marginBottom: '2rem',  height: '5.5rem' }}>
               <ThemeIcon className='find-Icon' style={{ alignContent: 'center' }} radius={360} size={'lg'} color={'none'}>
                 <IconPlus />
               </ThemeIcon>
               <h3><span className='teko'>ERSTELLEN</span></h3>
             </Link>
             
-            <Link to='/my-courses' className='options-Settings' style={{ backgroundColor: '#b1b2b5' }} onClick={() => setShowHomepageContent(false)}>
+            <Link to='/my-courses' className='options-Settings' style={{ backgroundColor: '#b1b2b5' }}>
               <ThemeIcon style={{ alignContent: 'center' }} radius={360} size={'lg'} color={'none'}>
                 <IconFile />
               </ThemeIcon>  
@@ -42,15 +53,6 @@ export default function Homepage() {
             </Link>
           </div>
         </div> 
-        )}
-
-        <Routes>
-          <Route path='/create-course' element={<CreateCourse/>} />
-          <Route path='/my-courses' element={<MyCourses/>}/>
-          <Route path='/edit-course' element={<EditCourse/>}/>
-          <Route path='/course-view' element={<CourseViewLayout/>}/>
-          <Route path='/course-pdf' element={<ExportPDF/>}/>
-        </Routes>
-    </Router>
-    );
-}
+      )}
+      
+export default CourseClockRouter;
